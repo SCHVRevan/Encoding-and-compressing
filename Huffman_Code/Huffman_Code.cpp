dcdea0d0a55876208d2f3cf2981269bd62eea3a1
Egor <120785170+SCHVRevan@users.noreply.github.com>
2022-12-28 03:00:53 +0700

    Update Huffman_Code.cpp

--- Huffman_Code/Huffman_Code.cpp
+++ Huffman_Code/Huffman_Code.cpp
@@ -3,33 +3,52 @@
 #include <queue>
 #include <map>
 #include <string>
-// узел бинарного дерева
+
+// узел дерева
 class Node() {
-  
+public:
+	char ch;	// Символ текста
+	int freq;	// Частота символа
+	Node *left, *right;	// Указатели на последователей
 };
+
+// Выделение нового узла
+Node *allocNode(char ch, int freq, Node *left, Node *right) {
+	Node* node = new Node();	// Выделяем память под новый узел
+	// Заполняем её соотв. элементами
+	node->ch = ch;
+	node->freq = freq;
+	node->left = left;
+	node->right = right;
+	return node;
+}
+
 // кодируем
 void encode() {
-  
+ 	
 }
 // декодируем
 void decode() {
-  
+ 	
 }
 // дерево Хаффмана (для декодирования шифртекста)
 void HuffmanTree() {
-  
+ 	
 }
 
 int main() {
-  // открываем файл для чтения
-  ifstream file_in("in_text.txt");
-  string text, tmp;
-  // пока не достигли конца файла, построчно считываем данные
+	// Открываем файл для чтения
+	ifstream file_in("in_text.txt");
+	// Открываем файлы для записи
+	ofstream file_out("out_text.txt"), file_coded("encoded_text.txt"), file_codes("Alphabet.txt");
+	string text, tmp;
+	// Пока не достигли конца файла, построчно считываем данные
 	while (!file_in.eof()) {
-		getline(file_in, tmp);  // т.к. функция getline() считывает до символа конца строки,
-		text += tmp + '\n';     // не включая их, дополняем текст этим элементов
+		getline(file_in, tmp);	// Т.к. функция getline() считывает до символа конца строки,
+		text += tmp + '\n';	// Не включая их, дополняем текст этим элементов
 	};
-	text.pop_back();          // исключаем лишний символ конца строки (\n)
-  file_in.close();          // закрываем файл
+	text.pop_back();	// Исключаем лишний символ конца строки (\n)
+  	file_in.close(), file_out.close(), file_coded.close(), file_codes.close();	// Закрываем файлы
+	
   return 0;
 }
